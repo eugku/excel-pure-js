5e34f081e6c0ebaa6697ed4c58b7698f21fc100d
eugku <yevgeniykurilo@gmail.com>
2020-09-22 16:10:42 +0200

    Implement Dependency Inversion Principle

--- src/core/routes/Router.spec.js
+++ src/core/routes/Router.spec.js
@@ -1,5 +1,5 @@
 import {Router} from './Router';
-import {Page} from '../Page';
+import {Page} from '../page/Page';
 
 class DashboardPage extends Page {
     getRoot() {
@@ -26,7 +26,7 @@ describe('Router', () => {
         expect(router).toBeDefined()
     })
 
-    test('should render Dashboard Page', () => {
+    test('should render Dashboard page', () => {
         router.changePageHandler()
         expect($root.innerHTML).toBe('<div>dashboard</div>')
     })
